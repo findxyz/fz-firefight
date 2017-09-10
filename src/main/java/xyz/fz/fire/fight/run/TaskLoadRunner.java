23b6bfe90fcd86b8bb081071fb51ee74a6bf3137
fz <natureorz@foxmail.com>
2017-09-10 18:28:57 +0800

    优化

--- src/main/java/xyz/fz/fire/fight/run/TaskLoadRunner.java
+++ src/main/java/xyz/fz/fire/fight/run/TaskLoadRunner.java
@@ -1,23 +1,21 @@
 package xyz.fz.fire.fight.run;
 
-import org.apache.commons.io.FileUtils;
-import org.springframework.beans.factory.annotation.Value;
+import org.apache.commons.io.IOUtils;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 import xyz.fz.fire.fight.model.Task;
 import xyz.fz.fire.fight.model.TaskHelper;
 import xyz.fz.fire.fight.util.BaseUtil;
 
+import java.io.InputStream;
+
 @Component
 public class TaskLoadRunner implements CommandLineRunner {
 
-    @Value(value = "classpath:task.json")
-    private Resource taskResource;
-
     @Override
     public void run(String... strings) throws Exception {
-        String taskJson = FileUtils.readFileToString(taskResource.getFile());
+        InputStream is = this.getClass().getResourceAsStream("/task.json");
+        String taskJson = IOUtils.toString(is);
         Task task = BaseUtil.parseJson(taskJson, Task.class);
         TaskHelper.taskEasy(task);
     }
